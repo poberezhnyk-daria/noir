f20c8c23e2cc29c309abe069b714d3255d5ba6f9
poberezhnyk-daria <poberezhnyk.daria@stud.onu.edu.ua>
2026-06-07 20:32:52 +0300

    Refactor App component to use React.FC type

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Menu from './components/Menu'
-import Booking from './components/Booking'
-import Reviews from './components/Reviews'
-import Footer from './components/Footer'
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Menu from './components/Menu';
+import Booking from './components/Booking';
+import Reviews from './components/Reviews';
+import Footer from './components/Footer';
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <>
       <Header />
@@ -17,7 +18,7 @@ const App = () => {
       </main>
       <Footer />
     </>
-  )
-}
+  );
+};
 
 export default App;
