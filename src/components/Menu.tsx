7809cfee50d44171f37cf89442c2e07f0d47078f
poberezhnyk-daria <poberezhnyk.daria@stud.onu.edu.ua>
2026-06-07 22:58:47 +0300

    Fix typo in menu image source filename

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Menu: React.FC = () => {
-  const menuImages = ['/menu1.JPG', '/menu2.JPG', '/menu3.JPG'];
+  const menuImages = ['/mebu1.JPG', '/menu2.JPG', '/menu3.JPG'];
 
   return (
     <section
