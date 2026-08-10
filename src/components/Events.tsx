7053a8cc229327d83639f90c1ef71027c590f652
poberezhnyk-daria <poberezhnyk.daria@stud.onu.edu.ua>
2026-08-10 23:51:11 +0300

    Refactor Events component and styles

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -19,7 +19,7 @@ const events: EventItem[] = [
   {
     title: 'MAFIA GAME',
     description: 'Легендарна психологічна гра, де маски стають реальністю. Випробуйте свою інтуїцію, логіку та акторську майстерність у боротьбі за мирне місто або станьте на бік таємничої мафії. Вечір інтриг, стратегій та незабутніх знайомств.',
-    poster: '/mafia.JPG',',
+    poster: '/mafia.JPG',
     photos: ['/maf1.JPG', '/maf2.JPG', '/maf3.JPG', 'public/maf4.JPG'],
     accentColor: '#A02C3D'
   }
