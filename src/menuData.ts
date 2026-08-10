09ca8c9965d382a4c834d9d403737317ba72e3df
poberezhnyk-daria <poberezhnyk.daria@stud.onu.edu.ua>
2026-08-11 00:10:20 +0300

    Update menuData.ts

--- src/menuData.ts
+++ src/menuData.ts
@@ -35,9 +35,8 @@ export const menuData: MenuData = {
           image: '/jezar-lococ.PNG'
         },
         { 
-          name: 'Салат Цезар з тунцем', 
-          description: '',
-          price: '250' 
+          name: 'Салат Цезар з тунцем',
+          price: '250 грн' 
         },
       ]
     },
